524746eb2398256c7035f6faad53b2d0e18c02fd
NguyenNguyen <nguyenbk92@gmail.com>
2020-01-30 22:05:24 +0700

    resolve #4: macd calculation

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 const rsi = require('./indicators/rsi.js')
 const sma = require('./indicators/sma.js')
 const bb = require('./indicators/bollinger_band.js')
+const macd = require('./indicators/macd.js')
 module.exports = {
     rsi: rsi,
     sma: sma,
     bb: bb,
+    macd: macd,
 }
 const main = async () => {
     console.log(await rsi(14, "close", "binance", "BTC/USDT", "15m", true))
+
     let smaData = await sma(8, "close", "binance", "BTC/USDT", "15m", true)
     console.log(smaData[smaData.length - 1])
+
     let bbData = await bb(50, 2, "close", "binance", "BTC/USDT", "15m", true)
-    console.log(bbData[bbData.length - 1])
+    console.log(bbData[bbData.length - 2])
+
+    let macdData = await macd(12, 26, 9, "close", "binance", "BTC/USDT", "15m", true)
+    console.log(macdData[macdData.length - 2])
 }
 main()
