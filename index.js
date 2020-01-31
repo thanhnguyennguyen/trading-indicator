644a268ccabea3a50ea9e8fd61bf18225fef053a
NguyenNguyen <nguyenbk92@gmail.com>
2020-01-31 08:34:30 +0700

    Remove debug log

--- index.js
+++ index.js
@@ -8,20 +8,20 @@ module.exports = {
     bb: bb,
     macd: macd,
 }
-const main = async () => {
-    console.log("RSI 14 on Binance BTC/USDT 15m")
-    console.log(await rsi(14, "close", "binance", "BTC/USDT", "15m", true))
-
-    console.log("SMA 8 on Binance BTC/USDT 15m")
-    let smaData = await sma(8, "close", "binance", "BTC/USDT", "15m", true)
-    console.log(smaData[smaData.length - 1])
-
-    console.log("Bollinger bands 50, 2 on Binance BTC/USDT 15m")
-    let bbData = await bb(50, 2, "close", "binance", "BTC/USDT", "15m", true)
-    console.log(bbData[bbData.length - 2])
-
-    console.log("MACD 12 26 9 on Binance BTC/USDT 15m")
-    let macdData = await macd(12, 26, 9, "close", "binance", "BTC/USDT", "15m", true)
-    console.log(macdData[macdData.length - 2])
-}
-main()
+// const main = async () => {
+//    console.log("RSI 14 on Binance BTC/USDT 15m")
+//    console.log(await rsi(14, "close", "binance", "BTC/USDT", "15m", true))
+// 
+//    console.log("SMA 8 on Binance BTC/USDT 15m")
+//    let smaData = await sma(8, "close", "binance", "BTC/USDT", "15m", true)
+//    console.log(smaData[smaData.length - 1])
+// 
+//    console.log("Bollinger bands 50, 2 on Binance BTC/USDT 15m")
+//    let bbData = await bb(50, 2, "close", "binance", "BTC/USDT", "15m", true)
+//    console.log(bbData[bbData.length - 2])
+// 
+//    console.log("MACD 12 26 9 on Binance BTC/USDT 15m")
+//    let macdData = await macd(12, 26, 9, "close", "binance", "BTC/USDT", "15m", true)
+//    console.log(macdData[macdData.length - 2])
+// 
+// main()
