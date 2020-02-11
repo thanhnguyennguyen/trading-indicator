907d23dfb962b45478749a49d21a920184c4aa45
NguyenNguyen <nguyenbk92@gmail.com>
2020-02-11 14:51:59 +0700

    resolve #17: MFI(Money Flow Index) value calculation

---
+++ indicators/mfi.js
@@ -0,0 +1,17 @@
+const getOHLCV = require('./ohlcv.js')
+const detachSource = require('./source.js')
+const indicators = require('technicalindicators')
+
+const mfi = async (mfiPeriod, ex, ticker, interval, isFuture = false) => {
+    let ohlcv = await getOHLCV(ex, ticker, interval, isFuture)
+    let source = detachSource(ohlcv)
+    let mfiInput = {
+	high: source['high'],
+	low: source['low'],
+        close: source['close'],
+        volume: source['volume'],
+	period: mfiPeriod,
+    }
+    return await indicators.MFI.calculate(mfiInput)
+}
+module.exports = mfi
