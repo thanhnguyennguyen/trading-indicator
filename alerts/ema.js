b08c092ab2ab08fe53e54573f2feb085304ebd6a
Mohamad Supangat <hantamkoding@gmail.com>
2022-02-08 14:08:37 +0700

    Fix all alert has error, and fixing index.js for testing

--- alerts/ema.js
+++ alerts/ema.js
@@ -1,70 +1,152 @@
-const ema = require('../indicators/ema.js')
-const getOHLCV = require('../indicators/ohlcv.js')
+const { ema } = require("../indicators/ema.js");
+const getOHLCV = require("../indicators/ohlcv.js");
 const {
-    crossover,
-    crossunder
-} = require('../utils/cross.js')
+  crossover,
+  crossunder,
+} = require("../utils/cross.js");
 
-const calculateEMA = async (EMA_FAST, EMA_SLOW, exchange, symbol, interval, isFuture) => {
-    try {
-        let EMA_FAST_VAL = await ema(parseInt(EMA_FAST), "close", exchange, symbol, interval, isFuture)
-        let EMA_SLOW_VAL = await ema(parseInt(EMA_SLOW), "close", exchange, symbol, interval, isFuture)
-        return {
-            fast: EMA_FAST_VAL,
-            slow: EMA_SLOW_VAL
-        }
-
-    } catch (err) {
-        throw (err)
-    }
-}
+const calculateEMA = async (
+  EMA_FAST,
+  EMA_SLOW,
+  exchange,
+  symbol,
+  interval,
+  isFuture,
+) => {
+  try {
+    let EMA_FAST_VAL = await ema(
+      parseInt(EMA_FAST),
+      "close",
+      exchange,
+      symbol,
+      interval,
+      isFuture,
+    );
+    let EMA_SLOW_VAL = await ema(
+      parseInt(EMA_SLOW),
+      "close",
+      exchange,
+      symbol,
+      interval,
+      isFuture,
+    );
+    return {
+      fast: EMA_FAST_VAL,
+      slow: EMA_SLOW_VAL,
+    };
+  } catch (err) {
+    throw (err);
+  }
+};
 
 //let emaFastVal, emaSlowVal
-const egoldenCross = async (EMA_FAST, EMA_SLOW, exchange, symbol, interval, isFuture = false) => {
-    var emaFastVal
-    var emaSlowVal
+const egoldenCross = async (
+  EMA_FAST,
+  EMA_SLOW,
+  exchange,
+  symbol,
+  interval,
+  isFuture = false,
+) => {
+  var emaFastVal;
+  var emaSlowVal;
 
-    //if (emaFastVal == undefined || emaSlowVal == undefined) {
-        let emaVal = await calculateEMA(EMA_FAST, EMA_SLOW, exchange, symbol, interval, isFuture)
-        emaFastVal = emaVal.fast
-        emaSlowVal = emaVal.slow
-    //}
+  //if (emaFastVal == undefined || emaSlowVal == undefined) {
+  let emaVal = await calculateEMA(
+    EMA_FAST,
+    EMA_SLOW,
+    exchange,
+    symbol,
+    interval,
+    isFuture,
+  );
+  emaFastVal = emaVal.fast;
+  emaSlowVal = emaVal.slow;
+  //}
 
-    return crossover(emaFastVal, emaSlowVal)
-}
+  return crossover(emaFastVal, emaSlowVal);
+};
 
-const edeathCross = async (EMA_FAST, EMA_SLOW, exchange, symbol, interval, isFuture = false) => {
-    var emaFastVal
-    var emaSlowVal
-    //if (emaFastVal == undefined || emaSlowVal == undefined) {
-        let emaVal = await calculateEMA(EMA_FAST, EMA_SLOW, exchange, symbol, interval, isFuture)
-        emaFastVal = emaVal.fast
-        emaSlowVal = emaVal.slow
-    //}
+const edeathCross = async (
+  EMA_FAST,
+  EMA_SLOW,
+  exchange,
+  symbol,
+  interval,
+  isFuture = false,
+) => {
+  var emaFastVal;
+  var emaSlowVal;
+  //if (emaFastVal == undefined || emaSlowVal == undefined) {
+  let emaVal = await calculateEMA(
+    EMA_FAST,
+    EMA_SLOW,
+    exchange,
+    symbol,
+    interval,
+    isFuture,
+  );
+  emaFastVal = emaVal.fast;
+  emaSlowVal = emaVal.slow;
+  //}
 
-    return crossunder(emaFastVal, emaSlowVal)
-}
+  return crossunder(emaFastVal, emaSlowVal);
+};
 
-const emaCross = async (EMA_FAST, EMA_SLOW, exchange, symbol, interval, isFuture = false) => {
-    return {
-        egoldenCross: await egoldenCross(EMA_FAST, EMA_SLOW, exchange, symbol, interval, isFuture),
-        edeathCross: await edeathCross(EMA_FAST, EMA_SLOW, exchange, symbol, interval, isFuture),
-    }
-}
+const emaCross = async (
+  EMA_FAST,
+  EMA_SLOW,
+  exchange,
+  symbol,
+  interval,
+  isFuture = false,
+) => {
+  return {
+    egoldenCross: await egoldenCross(
+      EMA_FAST,
+      EMA_SLOW,
+      exchange,
+      symbol,
+      interval,
+      isFuture,
+    ),
+    edeathCross: await edeathCross(
+      EMA_FAST,
+      EMA_SLOW,
+      exchange,
+      symbol,
+      interval,
+      isFuture,
+    ),
+  };
+};
 
-const priceCrossEMA = async (period, exchange, symbol, interval, isFuture = false) => {
-    let maVal = await ema(parseInt(period), "close", exchange, symbol, interval, isFuture),
-        ohlcv = await getOHLCV(exchange, symbol, interval, isFuture),
-        price = [ohlcv[1][3], ohlcv[0][3]],
-        up = crossover(price, maVal),
-        down = crossunder(price, maVal)
-    return {
-        "cross": up || down,
-        "direction": up ? "up" : (down ? "down" : "none"),
-    }
-}
+const priceCrossEMA = async (
+  period,
+  exchange,
+  symbol,
+  interval,
+  isFuture = false,
+) => {
+  let maVal = await ema(
+      parseInt(period),
+      "close",
+      exchange,
+      symbol,
+      interval,
+      isFuture,
+    ),
+    ohlcv = await getOHLCV(exchange, symbol, interval, isFuture),
+    price = [ohlcv[1][3], ohlcv[0][3]],
+    up = crossover(price, maVal),
+    down = crossunder(price, maVal);
+  return {
+    "cross": up || down,
+    "direction": up ? "up" : (down ? "down" : "none"),
+  };
+};
 
 module.exports = {
-    emaCross: emaCross,
-    priceCrossEMA: priceCrossEMA,
-}
+  emaCross: emaCross,
+  priceCrossEMA: priceCrossEMA,
+};
--- alerts/rsi.js
+++ alerts/rsi.js
@@ -1,21 +1,49 @@
-const rsi = require('../indicators/rsi.js')
-const calculateRSIValue = async (period, exchange, symbol, interval, isFuture) => {
-    try {
-        return await rsi(parseInt(period), "close", exchange, symbol, interval, isFuture)
-    } catch (err) {
-        throw (err)
-    }
-}
-const rsiCheck = async (period, overBoughtThreshold, overSoldThreshold, exchange, symbol, interval, isFuture = false) => {
-    let rsiVals = await calculateRSIValue(period, exchange, symbol, interval, isFuture),
-        rsiVal = rsiVals[rsiVals.length - 1]
-    return {
-        overBought: rsiVal >= overBoughtThreshold,
-        overSold: rsiVal <= overSoldThreshold,
-        rsiVal: rsiVal,
-    }
-}
+const { rsi } = require("../indicators/rsi.js");
+const calculateRSIValue = async (
+  period,
+  exchange,
+  symbol,
+  interval,
+  isFuture,
+) => {
+  try {
+    return await rsi(
+      parseInt(period),
+      "close",
+      exchange,
+      symbol,
+      interval,
+      isFuture,
+    );
+  } catch (err) {
+    throw (err);
+  }
+};
+const rsiCheck = async (
+  period,
+  overBoughtThreshold,
+  overSoldThreshold,
+  exchange,
+  symbol,
+  interval,
+  isFuture = false,
+) => {
+  let rsiVals = await calculateRSIValue(
+      period,
+      exchange,
+      symbol,
+      interval,
+      isFuture,
+    ),
+    rsiVal = rsiVals[rsiVals.length - 1];
+  return {
+    overBought: rsiVal >= overBoughtThreshold,
+    overSold: rsiVal <= overSoldThreshold,
+    rsiVal: rsiVal,
+  };
+};
 
 module.exports = {
-    rsiCheck: rsiCheck,
-}
+  rsiCheck: rsiCheck,
+};
+
